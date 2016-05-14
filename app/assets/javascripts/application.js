7963d2680827c6b49db6eb133461dadf0d7ae417
Igor Onischenko <optimum.flaminis@gmail.com>
2016-05-14 23:04:02 +0300

    some changes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require materialize-sprockets
-//= require_tree .
 $(document).ready(function(){
 	$(".button-collapse").sideNav();
 });
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,20 +1,20 @@
 class UserController < ApplicationController
 	before_action :getUser, only:[:show]
   def getUser
-  	@user = (params[:id])? User.find(params[:id]): User.find(session[:user_id]) 
+  	@user = (params[:id])? User.find(params[:id]): User.find(session[:user_id])
   end
   def show
   	if(@user.nil?) then redirect_to '/login' end
   	@projects = @user.projects
   	respond_to do |format|
   		format.json {
-  		    render json: @user.to_json(:except => [:password_digest, :salt], 
+  		    render json: @user.to_json(:except => [:password_digest, :salt],
   		    	# :include => [:projects])
 )
 
   		# 	render json: {
-  		# 			id:1, 
-  		# 			name: "you", 
+  		# 			id:1,
+  		# 			name: "you",
   		# 			surname: 'surname',
 				# 	experience: '20',
 				# 	role: 'user',
@@ -42,8 +42,8 @@ class UserController < ApplicationController
 
   def all
   	respond_to do |format|
-  		format.json { 
-  			render json: User.find(1).to_json(:except => [:password_digest, :salt], 
+  		format.json {
+  			render json: User.find(1).to_json(:except => [:password_digest, :salt],
   		    	:include => [:projects])
   		}
   		format.html{ redirect_to '/404.html' }
@@ -54,13 +54,13 @@ class UserController < ApplicationController
   def register
 
   end
-  
+
   def create
   	user = User.new(
 	  					name: params['name'],
 	  					surname: params['surname'],
 	  					email: params['email'],
-	  					password: params['password'], 
+	  					password: params['password'],
 	  					password_confirmation: params['repeat']
   					)
   	respond_to do |format|
@@ -78,7 +78,7 @@ class UserController < ApplicationController
 
   def login
   end
-  
+
   def auth
   	user = User.find_by_email(params[:email]).try(:authenticate, params[:password])
   	respond_to do |format|
