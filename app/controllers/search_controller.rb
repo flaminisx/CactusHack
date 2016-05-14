b6c18d0d10ae438e86128c2c1d8d2a6abfa7ebea
Igor Onischenko <optimum.flaminis@gmail.com>
2016-05-14 23:05:49 +0300

    merge

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,9 @@
 class SearchController < ApplicationController
   def index
+
+
+  	# User.where(tags.include?(tag))
+  	# Project.where(tags.include?(tag))
+  	# Team.where(tags.include?(tag))
   end
 end
