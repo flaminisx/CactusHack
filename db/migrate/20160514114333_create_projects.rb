8fe611755b9fa7f49bfced7e17bf38ec7efcd27d
AnichinDaniil <anichindaniil@gmail.com>
2016-05-14 14:47:59 +0300

    Created basic models

---
+++ db/migrate/20160514114333_create_projects.rb
@@ -0,0 +1,12 @@
+class CreateProjects < ActiveRecord::Migration
+  def change
+    create_table :projects do |t|
+      t.string :name
+      t.text :description
+      t.string :portfolio_reference
+      t.integer :owner_id
+
+      t.timestamps null: false
+    end
+  end
+end
