aca726a66108c62f311faf60f0bf5523518be628
Igor Onischenko <optimum.flaminis@gmail.com>
2016-05-15 02:43:52 +0300

    search v000.1

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,22 @@
 class SearchController < ApplicationController
   def index
+    @str = params[:str]
+    tags = params[:tag]
+    @data = []
+    if(!@str.nil?) then
+      @data = (User.where("name LIKE '" + @str+ "'").as_json.map { |e| e.merge!(type: "person") })
+      @data = @data | (Project.where("name LIKE '" + @str+ "'").as_json.map { |e| e.merge!(type: "project") })
+      @data = @data | (Team.where("name LIKE  '" + @str+ "'").as_json.map { |e| e.merge!(type: "team") })
+    end
+    if(!tags.nil?) then
+      tags = tags.split(',')
+      tags.map { |e|
+        @data |= Tag.where(tag: e).first.users
+        @data |= Tag.where(tag: e).first.teams
+        @data |= Tag.where(tag: e).first.projects
 
-
+      }
+    end
   	# User.where(tags.include?(tag))
   	# Project.where(tags.include?(tag))
   	# Team.where(tags.include?(tag))
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,9 @@
 class Tag < ActiveRecord::Base
+  has_many :user_tags
+  has_many :users, through: :user_tags
+  has_many :team_tags
+  has_many :teams, through: :team_tags
+  has_many :project_tags
+  has_many :projects, through: :project_tags
+
 end
